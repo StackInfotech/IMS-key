370cd76e7f6424bd415ba14aa840811add68b6c8
Praveen <pbpraveen1988@gmail.com>
2016-12-12 16:27:27 +0530

    complete code

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string macAddresses = "";
+            var s = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+
+                if (nic.NetworkInterfaceType == NetworkInterfaceType.Loopback) continue;
+                macAddresses += nic.GetPhysicalAddress().ToString();
+                break;
+            }
+
+            Console.WriteLine(macAddresses);
+            Console.Read();
+        }
+    }
+}
